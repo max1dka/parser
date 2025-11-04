d01d7b4893d6ba955de4b11e05b29542fc7121c9
max1dka <mitrofanov.m00@icloud.com>
2025-11-04 14:59:04 +0300

    fix comm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String logFilePath = "access (1).log"; // Имя вашего лог-файла
+        String logFilePath = "access (1).log"; // Имя лог-файла
         System.out.println("Обработка лог-файла: " + logFilePath);
         LogProcessor.processLogFile(logFilePath);
     }
